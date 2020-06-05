2f75003f63355679f6cb64dd13ef3854f07cab37
sujin-park <psujin831@gmail.com>
2020-06-05 22:19:38 +0900

    [U] social config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -5,7 +5,10 @@ module.exports = {
   introduction: `기록하는 습관. 📝`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
+    twitter: ``, // Your Twitter account
     github: `https://github.com/sujin-park`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``,
     linkedin: `https://www.linkedin.com/in/sujin-park-608008160/`, // Your LinkedIn account
   },
   icon: `content/assets/felog.png`, // Add your favicon
