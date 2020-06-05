fa158a9090605c3eac8301626392ec9c2de5a616
sujin-park <psujin831@gmail.com>
2020-06-05 22:12:11 +0900

    [A] gitignore

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,14 +1,11 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
+  title: `Sujin's Blog`,
+  description: ``,
   author: `sujin-park`,
-  introduction: ``,
+  introduction: `기록하는 습관. 📝`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
-    twitter: ``, // Your Twitter account
     github: `https://github.com/sujin-park`, // Your GitHub account
-    medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
     linkedin: `https://www.linkedin.com/in/sujin-park-608008160/`, // Your LinkedIn account
   },
   icon: `content/assets/felog.png`, // Add your favicon
